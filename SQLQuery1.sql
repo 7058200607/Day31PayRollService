fdb760cdfca20ee930e5acf10b33b85ee7e5f654
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-27 00:33:55 +0530

    calculate the sum avg min max of person in employee pay roll

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@
 create database Employee_service;
 --UC2
 use Employee_service;
-create table emp_payroll(empid int identity(1,1)primary key,empname varchar(200),salary bigint,gender varchar(200),start_date date);
+create table emp_payroll(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date,gender varchar(200));
 select*from emp_payroll;
 ---UC3
 insert into emp_payroll values('komal',20000,'2021-06-17');
@@ -20,3 +20,14 @@ select *from emp_payroll where  start_date between CAST('2019-03-01' as  date)an
 alter table emp_payroll2 add gender varchar(200);
 update emp_payroll set gender ='F'where empname='komal' or empname='mrunal'or empname='punam'or empname='aayushi';
 update emp_payroll set gender='M'where empname='sarthak'or empname='saurav';
+---uC7
+select SUM(salary) as TotalSalary from emp_payroll where gender='F'GROUP BY gender;
+select SUM(salary) as TotalSalary from emp_payroll where gender ='M'GROUP BY gender;
+select AVG(salary) as AvarageSalary from emp_payroll where gender ='F'GROUP BY gender;
+select AVG(salary) as AvarageSalary from emp_payroll where gender ='M'GROUP BY gender;
+select MIN(salary) as MinimumSalary from emp_payroll where gender ='F'GROUP BY gender;
+select MIN(salary) as MinimumSalary from emp_payroll where gender ='M'GROUP BY gender;
+select MAX(salary) as MaximumSalary from emp_payroll where gender ='F'GROUP BY gender;
+select MAX(salary) as MaximumSalary from emp_payroll where gender ='M'GROUP BY gender;
+select COUNT(salary) as CountSalary from emp_payroll where gender ='F'GROUP BY gender;
+select COUNT(salary) as AvarageSalary from emp_payroll where gender ='M'GROUP BY gender;
